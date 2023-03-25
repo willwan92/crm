1a85b3d39970eae0fa2d4084da70cd5a486f7541
willwan92 <wilson3cy@163.com>
2023-03-25 16:24:30 +0800

    修改登录页背景

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -16,7 +16,11 @@
           :rules="rules"
         >
           <n-form-item path="username">
-            <n-input v-model:value="formInline.username" ref="usernameRef" placeholder="请输入用户名">
+            <n-input
+              v-model:value="formInline.username"
+              ref="usernameRef"
+              placeholder="请输入用户名"
+            >
               <template #prefix>
                 <n-icon size="18" color="#808695">
                   <PersonOutline />
@@ -59,144 +63,146 @@
 </template>
 
 <script lang="ts" setup>
-import { reactive, ref,onMounted } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import { useUserStore } from '@/store/modules/user';
-import { useMessage } from 'naive-ui';
-import { ResultEnum } from '@/enums/httpEnum';
-import { PageEnum } from '@/enums/pageEnum';
-import { websiteConfig } from '@/config/website.config';
-import { storage } from '@/utils/Storage';
-import { TABS_ROUTES } from '@/store/mutation-types';
+  import { reactive, ref, onMounted } from 'vue';
+  import { useRoute, useRouter } from 'vue-router';
+  import { useUserStore } from '@/store/modules/user';
+  import { useMessage } from 'naive-ui';
+  import { ResultEnum } from '@/enums/httpEnum';
+  import { PageEnum } from '@/enums/pageEnum';
+  import { LockClosedOutline, PersonOutline } from '@vicons/ionicons5';
+  import { websiteConfig } from '@/config/website.config';
+  import { storage } from '@/utils/Storage';
+  import { TABS_ROUTES } from '@/store/mutation-types';
 
-interface FormState {
-  username: string;
-  password: string;
-  captcha: string;
-}
-const usernameRef = ref();
-const formRef = ref();
-const message = useMessage();
-const loading = ref(false);
-const LOGIN_NAME = PageEnum.BASE_LOGIN_NAME;
+  interface FormState {
+    username: string;
+    password: string;
+    captcha: string;
+  }
+  const usernameRef = ref();
+  const formRef = ref();
+  const message = useMessage();
+  const loading = ref(false);
+  const LOGIN_NAME = PageEnum.BASE_LOGIN_NAME;
 
-onMounted(()=>{
-  usernameRef.value.focus();
-})
-
-const formInline = reactive({
-  username: '',
-  password: '',
-  captcha: '',
-  captchaUrl: '/api/auth/captcha',
-});
-
-const rules = {
-  username: { required: true, message: '请输入用户名', trigger: 'blur' },
-  password: { required: true, message: '请输入密码', trigger: 'blur' },
-  captcha: { required: true, message: '请输入验证码', trigger: 'blur' },
-};
-
-const userStore = useUserStore();
-
-const router = useRouter();
-const route = useRoute();
-const refreshCaptach = () => {
-  formInline.captchaUrl = `/api/auth/captcha?t=${Date.now()}`;
-};
-const handleSubmit = (e) => {
-  e.preventDefault();
-  loginHandle();
-};
-const loginHandle = () => {
-  if (loading.value) return;
-  formRef.value.validate(async (errors) => {
-    if (!errors) {
-      const { username, password, captcha } = formInline;
-      message.loading('登录中...');
-      loading.value = true;
-      const params: FormState = {
-        username,
-        password,
-        captcha,
-      };
-      try {
-        const res = await userStore.login(params);
-        message.destroyAll();
-        if (res.code == ResultEnum.SUCCESS) {
-          storage.remove(TABS_ROUTES);
-          // const toPath = decodeURIComponent((route.query?.redirect || '/') as string);
-          message.success('登录成功，即将进入系统');
-          if (route.name === LOGIN_NAME) {
-            router.replace('/');
-          } else {
-            router.replace('/home/index');
-          }
-        }
-      } catch (error) {
-        refreshCaptach();
-      } finally {
-        loading.value = false;
-      }
-    } else {
-      message.error('请填写完整信息，并且进行验证码校验');
-    }
+  onMounted(() => {
+    usernameRef.value.focus();
   });
-}
+
+  const formInline = reactive({
+    username: '',
+    password: '',
+    captcha: '',
+    captchaUrl: '/api/auth/captcha',
+  });
+
+  const rules = {
+    username: { required: true, message: '请输入用户名', trigger: 'blur' },
+    password: { required: true, message: '请输入密码', trigger: 'blur' },
+    captcha: { required: true, message: '请输入验证码', trigger: 'blur' },
+  };
+
+  const userStore = useUserStore();
+
+  const router = useRouter();
+  const route = useRoute();
+  const refreshCaptach = () => {
+    formInline.captchaUrl = `/api/auth/captcha?t=${Date.now()}`;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loginHandle();
+  };
+  const loginHandle = () => {
+    if (loading.value) return;
+    formRef.value.validate(async (errors) => {
+      if (!errors) {
+        const { username, password, captcha } = formInline;
+        message.loading('登录中...');
+        loading.value = true;
+        const params: FormState = {
+          username,
+          password,
+          captcha,
+        };
+        try {
+          const res = await userStore.login(params);
+          message.destroyAll();
+          if (res.code == ResultEnum.SUCCESS) {
+            storage.remove(TABS_ROUTES);
+            // const toPath = decodeURIComponent((route.query?.redirect || '/') as string);
+            message.success('登录成功，即将进入系统');
+            if (route.name === LOGIN_NAME) {
+              router.replace('/');
+            } else {
+              router.replace('/home/index');
+            }
+          }
+        } catch (error) {
+          refreshCaptach();
+        } finally {
+          loading.value = false;
+        }
+      } else {
+        message.error('请填写完整信息，并且进行验证码校验');
+      }
+    });
+  };
 </script>
 
 <style lang="less" scoped>
-.view-account {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  overflow: auto;
-
-  &-container {
-    flex: 1;
-    padding: 32px 12px;
-    max-width: 384px;
-    min-width: 320px;
-    margin: 0 auto;
-  }
-
-  &-top {
-    padding: 32px 0;
-    text-align: center;
-    margin-bottom: 30px;
-
-    &-logo img {
-      display: inline-block;
-      width: 240px;
-    }
-  }
-
-  &-other {
-    width: 100%;
-  }
-
-  .default-color {
-    color: #515a6e;
-    .ant-checkbox-wrapper {
-      color: #515a6e;
-    }
-  }
-  .captcha-img {
-    width: 110px;
-    margin-left: 10px;
-  }
-}
-
-@media (min-width: 768px) {
   .view-account {
-    background-image: url('../../assets/images/login.svg');
-    background-repeat: no-repeat;
-    background-position: 50%;
-    background-size: 100%;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    overflow: auto;
+
+    &-container {
+      flex: 1;
+      padding: 32px 12px;
+      max-width: 384px;
+      min-width: 320px;
+      margin: 0 auto;
+    }
+
+    &-top {
+      padding: 32px 0;
+      text-align: center;
+      margin-bottom: 60px;
+      margin-top: 120px;
+
+      &-logo img {
+        display: inline-block;
+        width: 240px;
+      }
+    }
+
+    &-other {
+      width: 100%;
+    }
+
+    .default-color {
+      color: #515a6e;
+      .ant-checkbox-wrapper {
+        color: #515a6e;
+      }
+    }
+    .captcha-img {
+      width: 110px;
+      margin-left: 10px;
+    }
   }
 
-  .page-account-container {
-    padding: 32px 0 24px 0;
+  @media (min-width: 768px) {
+    .view-account {
+      background-image: url('../../assets/images/login-bg-blue.jpg');
+      background-repeat: no-repeat;
+      background-position: 50%;
+      background-size: cover;
+    }
+
+    .page-account-container {
+      padding: 32px 0 24px 0;
+    }
   }
-}
 </style>
