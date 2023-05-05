ecac70ad5b74c93dd1bd477bd02a6d17026ad9bf
willwan92 <wilson3cy@163.com>
2023-05-05 17:44:24 +0800

    修改客户编辑页

--- src/views/customer/components/EditModal.vue
+++ src/views/customer/components/EditModal.vue
@@ -31,87 +31,57 @@
           </n-form-item>
         </n-gi>
         <n-gi>
+          <n-form-item label="客户名称" path="customerName">
+            <n-input v-model:value="formParams.customerName" />
+          </n-form-item>
+        </n-gi>
+        <n-gi>
           <n-form-item label="客户来源" path="customerSource">
             <n-select
               v-model:value="formParams.customerSource"
               label-field="label"
               value-field="label"
-              :disabled="Boolean(customerId)"
               :options="customerSourceOptions"
             />
           </n-form-item>
         </n-gi>
         <n-gi>
-          <n-form-item label="客户名称" path="customerName">
-            <n-input v-model:value="formParams.customerName" :disabled="Boolean(customerId)" />
-          </n-form-item>
-        </n-gi>
-        <n-gi>
-          <n-form-item label="组织机构代码" path="organizationCode">
-            <n-input v-model:value="formParams.organizationCode" :disabled="Boolean(customerId)" />
-          </n-form-item>
-        </n-gi>
-        <n-gi v-show="!customerId">
-          <n-form-item label="客户所在地区" path="areaId">
+          <n-form-item label="客户所在地区" path="mergerName">
             <n-cascader
               remote
               check-strategy="child"
-              v-model:value="formParams.areaId"
+              v-model:value="formParams.mergerName"
               :options="addrOptions"
               :on-load="handleAreaLoad"
               :on-update:value="handleAreaIdChange"
             />
           </n-form-item>
         </n-gi>
-        <n-gi v-show="Boolean(customerId)">
-          <n-form-item label="客户所在地区" path="mergerName">
-            <n-input v-model:value="formParams.mergerName" :disabled="Boolean(customerId)" />
+        <n-gi>
+          <n-form-item label="需求业态" path="unitType">
+            <n-radio-group v-model:value="formParams.unitType">
+              <n-space>
+                <n-radio value="独栋">独栋</n-radio>
+                <n-radio value="分层">分层</n-radio>
+              </n-space>
+            </n-radio-group>
           </n-form-item>
         </n-gi>
         <n-gi>
-          <n-form-item label="注册地址" path="registerAddress">
-            <n-input v-model:value="formParams.registerAddress" :disabled="Boolean(customerId)" />
-          </n-form-item>
-        </n-gi>
-        <n-gi>
-          <n-form-item label="注册资本" path="registerCapital">
-            <n-input-group>
-              <n-input v-model:value="formParams.registerCapital" :disabled="Boolean(customerId)" />
-              <n-select
-                v-model:value="monetaryUnit"
-                label-field="label"
-                value-field="label"
-                :disabled="Boolean(customerId)"
-                :options="unitOptions"
-                :consistent-menu-width="false"
-              />
-            </n-input-group>
-          </n-form-item>
-        </n-gi>
-        <n-gi>
-          <n-form-item label="注册时间" path="registerTime">
-            <n-date-picker
-              v-model:value="formParams.registerTime"
-              type="date"
-              clearable
-              :disabled="Boolean(customerId)"
-            />
-          </n-form-item>
-        </n-gi>
-        <n-gi>
-          <n-form-item label="关联企业" path="relaEnterprises">
-            <n-select
-              v-model:value="formParams.relaEnterprises"
-              label-field="label"
-              value-field="label"
-              :options="relaEnterprisesOptions"
-              :disabled="Boolean(customerId)"
-            />
+          <n-form-item label="需求面积" path="requireArea">
+            <n-input v-model:value="formParams.requireArea">
+              <template #suffix> 平米 </template>
+            </n-input>
           </n-form-item>
         </n-gi>
         <n-gi>
           <n-form-item label="联系人" path="contactName">
             <n-input v-model:value="formParams.contactName" :disabled="Boolean(customerId)" />
+          </n-form-item>
+        </n-gi>
+        <n-gi>
+          <n-form-item label="联系电话" path="contactPhone">
+            <n-input v-model:value="formParams.contactPhone" :disabled="Boolean(customerId)" />
           </n-form-item>
         </n-gi>
         <n-gi>
@@ -121,13 +91,46 @@
               label-field="label"
               value-field="label"
               :options="contactPositionOptions"
-              :disabled="Boolean(customerId)"
             />
           </n-form-item>
         </n-gi>
         <n-gi>
-          <n-form-item label="联系电话" path="contactPhone">
-            <n-input v-model:value="formParams.contactPhone" :disabled="Boolean(customerId)" />
+          <n-form-item label="组织机构代码" path="organizationCode">
+            <n-input v-model:value="formParams.organizationCode" />
+          </n-form-item>
+        </n-gi>
+        <n-gi>
+          <n-form-item label="注册地址" path="registerAddress">
+            <n-input v-model:value="formParams.registerAddress" />
+          </n-form-item>
+        </n-gi>
+        <n-gi>
+          <n-form-item label="注册资本" path="registerCapital">
+            <n-input-group>
+              <n-input v-model:value="formParams.registerCapital" />
+              <n-select
+                v-model:value="monetaryUnit"
+                label-field="label"
+                value-field="label"
+                :options="unitOptions"
+                :consistent-menu-width="false"
+              />
+            </n-input-group>
+          </n-form-item>
+        </n-gi>
+        <n-gi>
+          <n-form-item label="注册时间" path="registerTime">
+            <n-date-picker v-model:value="formParams.registerTime" type="date" clearable />
+          </n-form-item>
+        </n-gi>
+        <n-gi>
+          <n-form-item label="关联企业" path="relaEnterprises">
+            <n-select
+              v-model:value="formParams.relaEnterprises"
+              label-field="label"
+              value-field="label"
+              :options="relaEnterprisesOptions"
+            />
           </n-form-item>
         </n-gi>
         <n-gi>
@@ -171,23 +174,6 @@
             </n-radio-group>
           </n-form-item>
         </n-gi>
-        <n-gi>
-          <n-form-item label="需求业态" path="unitType">
-            <n-radio-group v-model:value="formParams.unitType">
-              <n-space>
-                <n-radio value="独栋">独栋</n-radio>
-                <n-radio value="分层">分层</n-radio>
-              </n-space>
-            </n-radio-group>
-          </n-form-item>
-        </n-gi>
-        <n-gi>
-          <n-form-item label="需求面积" path="requireArea">
-            <n-input v-model:value="formParams.requireArea">
-              <template #suffix> 平米 </template>
-            </n-input>
-          </n-form-item>
-        </n-gi>
       </n-grid>
     </n-form>
 
@@ -201,7 +187,7 @@
 </template>
 
 <script lang="ts" setup>
-  import { reactive, ref, unref, defineExpose, defineEmits } from 'vue';
+  import { reactive, ref, unref } from 'vue';
   import { FormRules, FormItemRule } from 'naive-ui';
   import {
     unitOptions,
@@ -291,34 +277,39 @@
   const emit = defineEmits(['ok']);
   const rules = reactive<FormRules>({
     accountResourceId: { required: true, trigger: ['blur', 'input'], message: '请选择招商项目' },
-    areaId: { required: true, trigger: ['change'], message: '请选择客户所在省市区' },
-    rentType: { required: true, trigger: ['blur', 'input'], message: '请选择租售类型' },
+    mergerName: { required: true, trigger: ['change'], message: '请选择客户所在省市区' },
+    rentType: { required: false, trigger: ['blur', 'input'], message: '请选择租售类型' },
     customerSource: { required: true, trigger: ['blur', 'input'], message: '请选择客户来源' },
     customerName: { required: true, trigger: ['blur', 'input'], message: '请输入客户名称' },
-    organizationCode: { required: true, trigger: ['blur', 'input'], message: '请输入组织机构代码' },
-    registerAddress: { required: true, trigger: ['blur', 'input'], message: '请输入注册地址' },
+    organizationCode: {
+      required: false,
+      trigger: ['blur', 'input'],
+      message: '请输入组织机构代码',
+    },
+    registerAddress: { required: false, trigger: ['blur', 'input'], message: '请输入注册地址' },
     registerCapital: [
       {
-        required: true,
+        required: false,
         trigger: ['blur', 'input'],
         validator(rule: FormItemRule, value: string) {
-          if (!value) {
-            return new Error('请输入注册资本');
-          } else {
-            if (!POSITIVE_INT_REGEXP.test(value)) {
-              return new Error('只能输入正整数');
-            }
+          if (value && !POSITIVE_INT_REGEXP.test(value)) {
+            return new Error('只能输入正整数');
           }
         },
       },
     ],
-    registerTime: { required: true, type: 'integer', trigger: 'change', message: '请输入注册时间' },
+    registerTime: {
+      required: false,
+      type: 'integer',
+      trigger: 'change',
+      message: '请输入注册时间',
+    },
     contactName: { required: true, trigger: ['blur', 'input'], message: '请输入联系人' },
     contactPhone: [
       { required: true, trigger: ['blur', 'input'], message: '请输入联系人电话' },
       { pattern: MOBILE_REGEXP, trigger: ['blur', 'input'], message: '手机号格式错误' },
     ],
-    mainProduct: { required: true, trigger: ['blur', 'input'], message: '请输入主营产品' },
+    mainProduct: { required: false, trigger: ['blur', 'input'], message: '请输入主营产品' },
     requireArea: {
       required: true,
       trigger: ['blur', 'input'],
@@ -333,21 +324,17 @@
       },
     },
     spaceCondition: {
-      required: true,
+      required: false,
       trigger: ['blur', 'input'],
       validator(rule: FormItemRule, value: string) {
-        if (!value) {
-          return new Error('请输入现有场地面积');
-        } else {
-          if (!POSITIVE_INT_REGEXP.test(value)) {
-            return new Error('只能输入正整数');
-          }
+        if (value && !POSITIVE_INT_REGEXP.test(value)) {
+          return new Error('只能输入正整数');
         }
       },
     },
-    contactPosition: { required: true, trigger: ['blur', 'input'], message: '请选择联系人职务' },
-    industry: { required: true, trigger: ['blur', 'input'], message: '请选择所属行业' },
-    relaEnterprises: { required: true, trigger: ['blur', 'input'], message: '请选择关联企业数量' },
+    contactPosition: { required: false, trigger: ['blur', 'input'], message: '请选择联系人职务' },
+    industry: { required: false, trigger: ['blur', 'input'], message: '请选择所属行业' },
+    relaEnterprises: { required: false, trigger: ['blur', 'input'], message: '请选择关联企业数量' },
     unitType: { required: true, trigger: ['blur', 'input'], message: '请选择需求业态' },
   });
 
@@ -366,19 +353,25 @@
       customerId.value = id;
       getDetail(id).then((res) => {
         Object.assign(unref(formParams), res.baseInfoDTO);
-        formParams.areaId = '1';
-        const registerCapital = formParams.registerCapital.split('万');
-        formParams.registerCapital = registerCapital[0];
-        monetaryUnit.value = `万${registerCapital[1]}`;
 
-        const spaceCondition = formParams.spaceCondition.split(' ');
-        formParams.spaceCondition = spaceCondition[1];
-        spaceConditionType.value = spaceCondition[0];
+        if (formParams.registerCapital) {
+          const registerCapital = formParams.registerCapital.split('万');
+          formParams.registerCapital = registerCapital[0];
+          monetaryUnit.value = `万${registerCapital[1]}`;
+        } else {
+          monetaryUnit.value = '万（元）';
+        }
+
+        if (formParams.spaceCondition) {
+          const spaceCondition = formParams.spaceCondition.split(' ');
+          formParams.spaceCondition = spaceCondition[1];
+          spaceConditionType.value = spaceCondition[0];
+        } else {
+          spaceConditionType.value = '租赁';
+        }
       });
     } else {
       customerId.value = '';
-      monetaryUnit.value = '万（元）';
-      spaceConditionType.value = '租赁';
       formParams = Object.assign(unref(formParams), defaultParams());
     }
   };
@@ -390,10 +383,14 @@
         return;
       }
       isConfirming.value = true;
+      const params: any = Object.assign({}, formParams);
+      params.registerCapital = params.registerCapital
+        ? `${params.registerCapital}${monetaryUnit.value}`
+        : '';
+      params.spaceCondition = formParams.spaceCondition
+        ? `${spaceConditionType.value} ${formParams.spaceCondition}`
+        : '';
       if (!customerId.value) {
-        const params = Object.assign({}, formParams);
-        params.registerCapital += `${monetaryUnit.value}`;
-        params.spaceCondition = `${spaceConditionType.value} ${formParams.spaceCondition}`;
         add(params)
           .then(() => {
             modalVisible.value = false;
@@ -403,15 +400,11 @@
             isConfirming.value = false;
           });
       } else {
-        const params = {
-          customerId: customerId.value,
-          industry: formParams.industry,
-          mainProduct: formParams.mainProduct,
-          rentType: formParams.rentType,
-          requireArea: formParams.requireArea,
-          spaceCondition: `${spaceConditionType.value} ${formParams.spaceCondition}`,
-          unitType: formParams.unitType,
-        };
+        params.customerId = customerId.value;
+        Reflect.deleteProperty(params, 'accountResourceId');
+        Reflect.deleteProperty(params, 'accountResourceName');
+        Reflect.deleteProperty(params, 'contactName');
+        Reflect.deleteProperty(params, 'contactPhone');
 
         update(params)
           .then(() => {
