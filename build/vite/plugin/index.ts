59bd1a504e29efe867af50b5c12793632be3c1b8
willwan92 <wilson3cy@163.com>
2023-06-14 10:10:04 +0800

    修改pwa配置

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -55,27 +55,27 @@ export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean, prodMock) 
         ],
       },
       registerType: 'autoUpdate',
-      //   workbox: {
-      //     globPatterns: ['**/*.{js,css,html,ico,png,svg}'], //缓存相关静态资源
-      //     runtimeCaching: [
-      //       {
-      //         //由于要测试第三方API， 这里配置缓存访问第三方API的资源
-      //         handler: 'CacheFirst',
-      //         urlPattern: /^https:\/\/jsonplaceholder/, //注意，要修改成要测试的API。请使用正则表达式匹配
-      //         method: 'GET',
-      //         options: {
-      //           cacheName: 'test-external-api', //创建缓存名称
-      //           expiration: {
-      //             maxEntries: 10,
-      //             maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-      //           },
-      //           cacheableResponse: {
-      //             statuses: [0, 200],
-      //           },
-      //         },
-      //       },
-      //     ],
-      //   },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'], //缓存相关静态资源
+        //     runtimeCaching: [
+        //       {
+        //         //由于要测试第三方API， 这里配置缓存访问第三方API的资源
+        //         handler: 'CacheFirst',
+        //         urlPattern: /^https:\/\/jsonplaceholder/, //注意，要修改成要测试的API。请使用正则表达式匹配
+        //         method: 'GET',
+        //         options: {
+        //           cacheName: 'test-external-api', //创建缓存名称
+        //           expiration: {
+        //             maxEntries: 10,
+        //             maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+        //           },
+        //           cacheableResponse: {
+        //             statuses: [0, 200],
+        //           },
+        //         },
+        //       },
+        //     ],
+      },
       devOptions: {
         enabled: true,
       },
