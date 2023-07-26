83587cbe003be9850c3a95e5c20381675f707208
willwan92 <wilson3cy@163.com>
2023-07-26 14:24:19 +0800

    更新sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hp863ld8kuo"
+    "revision": "0.5evjj9vpug8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
